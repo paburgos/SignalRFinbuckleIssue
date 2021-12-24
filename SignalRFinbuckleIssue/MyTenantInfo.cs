4d65892d5dd6a317e2ea27cca254366f4b4c956b
paburgos <paburgos@hotmail.com>
2021-12-23 19:45:32 -0500

    commented signalr

---
+++ SignalRFinbuckleIssue/MyTenantInfo.cs
@@ -0,0 +1,9 @@
+﻿using Finbuckle.MultiTenant;
+
+public class MyTenantInfo : ITenantInfo
+{
+    public string? Id { get; set; }
+    public string? Identifier { get; set; }
+    public string? Name { get; set; }
+    public string? ConnectionString { get; set; }
+}
